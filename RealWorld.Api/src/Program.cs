fdf91313440b3508a6d10338fcb5f684d0273ec2
Keshiba <imkeshiba@gmail.com>
2024-09-22 21:02:19 +0530

    Add service dependency

--- RealWorld.Api/src/Program.cs
+++ RealWorld.Api/src/Program.cs
@@ -4,6 +4,7 @@ using RealWorld.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
